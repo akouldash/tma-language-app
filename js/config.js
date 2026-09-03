9d2f135d85a91e77db82e010933a97f13054452e
akouldash <akouldash@gmail.com>
2026-09-03 07:25:17 +0330

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,6 +7,7 @@
 const N8N_AUTH_URL = "/api/n8n/auth"; 
 const N8N_DELETE_URL = "/api/n8n/delete-account";
 const N8N_PLACEMENT_URL = "/api/n8n/placement-test";
+const N8N_LESSON_WEBHOOK_URL = '/api/n8n/generate-lesson';
 
 // شناسه کاربر جاری تلگرام
 let currentUserTelegramId = null;
